4956ed4da14d742593f65b32dd9cb36b94fc93aa
tyler yazzie <apple@Apples-MacBook-Pro.local>
2018-08-07 11:28:56 -0700

    finished with additional comments

--- part1/server.js
+++ part1/server.js
@@ -4,34 +4,54 @@ var path = require('path');
 var port = process.env.PORT || 8000;
 
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
-})
+// // ### Challenge 3:
+// // Create a GET route for "/" that returns the index.html file.
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/index.html')); //get request to "/"
+// })
 
-app.get('/hello', function(req, res) {
-  res.send("Hello");
+
+
+// ### Challenge 1:
+// Create a GET route for "/hello" that returns the response "Hello!".
+app.get('/hello', function(req, res) { //get request to /hello
+  res.send("Hello"); //say "hello"
 });
 
-app.get('/verify/:age', function(req, res){
-  let age = parseInt(req.params.age);
-  if(age > 13) {
-    res.sendStatus(200);
-  }else{
-    res.sendStatus(403);
-  }
-})
-
-app.post('/create/:name', function(req, res){
-  res.json({
-    id: 1,
-    name: req.params.name
-  })
-})
-
-app.use(function(req, res) {
-  res.sendStatus(404);
-});
-
-app.listen(port, function() {
-  console.log('Listening on port', port);
-});
+// 
+//
+//
+// // ### Challenge 4:
+// // Create a GET route for "/verify/:age" that checks if the age is greater than 13. If it is then it sends a 200 status response (for all good). If it is not greater than 13 then send a 403 status code.
+// // For more information on what status codes mean refer to this - https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+// app.get('/verify/:age', function(req, res) {
+//   let age = parseInt(req.params.age);
+//   if (age > 13) {
+//     res.sendStatus(200);
+//   } else {
+//     res.sendStatus(403);
+//   }
+// })
+//
+//
+//
+// // ### Challenge 2:
+// // Create a POST route for "/create/:name" that returns a json object that looks like this:
+// // {
+// //   "id":1,
+// //   "name":"the name they pass in the route"
+// // }
+// app.post('/create/:name', function(req, res) {
+//   res.json({
+//     id: 1,
+//     name: req.params.name
+//   })
+// })
+//
+// app.use(function(req, res) {
+//   res.sendStatus(404);
+// });
+//
+// app.listen(port, function() {
+//   console.log('Listening on port', port);
+// });
--- part2/server.js
+++ part2/server.js
@@ -3,43 +3,65 @@ var app = express();
 var fs = require('fs')
 var port = process.env.PORT || 8000;
 
-app.get('/', function(req,res){
-  fs.readFile('./storage.json', 'utf8', function(err, data){
-    res.json(JSON.parse(data));
+
+
+// ### Challenge 2:
+// Create a Get route for "/" that returns all of the objects inside storage.json.
+app.get('/', function(req, res) {
+  fs.readFile('./storage.json', 'utf8', function(err, data) { //want to read the file by "fs.readFile"
+    res.json(JSON.parse(data)); //'JSON.parse(data)' makes it a valid javascript array
   })
 })
+//REMINDER - 'fs.readFile' makes it a big string
+//'JSON.parse', parses it to make a  valid javascript array
 
-app.get('/:name', function(req,res){
-  fs.readFile('./storage.json', 'utf8', function(err, data){
-    let parsedData = JSON.parse(data);
-    let matchedUser = parsedData.filter((item)=>{
-      return item.name == req.params.name;
+
+
+// ### Challenge 3:
+// Create a Get route for "/:name" that returns the first object in storage.json that matches the name. If there
+// is no object in storage.json that matches then return a 400 status.
+app.get('/:name', function(req, res) {
+  fs.readFile('./storage.json', 'utf8', function(err, data) { //first we wan to read the file "fs.readFile"
+    let parsedData = JSON.parse(data); //'JSON.parse' - parses the data into a valid javascipt array
+    let matchedUser = parsedData.filter((item) => {
+      return item.name == req.params.name; //if we find a user with the same name, then return them
     });
 
-    if(matchedUser.length>=1) {
+    if (matchedUser.length >= 1) { //grabbing only '1 user' that matches 
       res.json(matchedUser[0]);
-    }else{
+    } else {
       res.sendStatus(400);
     }
   })
 })
 
+
+// ### Challenge 1:
+// Create a POST route for "/create/:name/:age" that creates an object that looks like this:
+// {
+//   "name": "troy",
+//   "age": 20
+// }
+// Then take that object and insert it into storage.json
 app.post('/create/:name/:age', function(req, res) {
   let newObj = {
     name: req.params.name,
-    age: parseInt(req.params.age)
+    age: parseInt(req.params.age) //parseInt - parses a string argument and returns an integer of the specified
+    //radix (the base in mathematical numeral systems).
   }
 
-   fs.readFile('./storage.json', 'utf8', function(err, data){
-     let dataAsArr = JSON.parse(data);
+  fs.readFile('./storage.json', 'utf8', function(err, data) { //parse the data into a  valid javascript array
+    let dataAsArr = JSON.parse(data);
 
-      dataAsArr.push(newObj);
+    dataAsArr.push(newObj); //push the data into the 'newObj'
 
-      fs.writeFile('./storage.json', JSON.stringify(dataAsArr), function(err){
-        res.sendStatus(200);
-      })
-   })
+    fs.writeFile('./storage.json', JSON.stringify(dataAsArr), function(err) { //using 'asynchronous' version on 'writeFile'
+      res.sendStatus(200); //'json.stringify' to convert data in an array
+    })
+  })
 });
+
+
 
 
 
